6455c0bf671e8b7e6e43aec8815bca692a471481
muhammadrifqialimin <rifqi.alimin.ft23@mail.umy.ac.id>
2025-10-16 17:31:54 +0700

    feat: menambahkan Box berwarna cyan dengan alignment center dan tinggi 300dp

--- app/src/main/java/com/example/praktikum2/TataLetak.kt
+++ app/src/main/java/com/example/praktikum2/TataLetak.kt
@@ -134,3 +134,9 @@ fun TataletakBoxColumnRow(modifier: Modifier) {
         }
         Spacer(modifier = Modifier.height( height = 10.dp))
         Box(
+            modifier = modifier
+                .fillMaxWidth()
+                .height( height = 300.dp)
+                .background(color = Color.Cyan),
+            contentAlignment = Alignment.Center
+        )
