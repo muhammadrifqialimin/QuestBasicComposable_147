96425e18f909599149c75136c99da252b77a299f
muhammadrifqialimin <rifqi.alimin.ft23@mail.umy.ac.id>
2025-10-16 16:15:32 +0700

    Menambahkan komponen tata letak Column, Row, dan Box pada Compose

--- app/src/main/java/com/example/praktikum2/TataLetak.kt
+++ app/src/main/java/com/example/praktikum2/TataLetak.kt
@@ -1,1 +1,60 @@
 package com.example.praktikum2
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
+@Composable
+fun TataLetakColumn(modifier: Modifier) {
+    Column(modifier = modifier.padding(top = 20.dp)) {
+        Text(text = "komponen1")
+        Text(text = "komponen2")
+        Text(text = "komponen3")
+        Text(text = "komponen4")
+    }
+}
+
+@Composable
+fun TataLetakRow(modifier: Modifier) {
+    Row (modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceEvenly) {
+        Text(text = "komponen1")
+        Text(text = "komponen2")
+        Text(text = "komponen3")
+        Text(text = "komponen4")
+    }
+}
+
+@Composable
+fun TataLetakBox(modifier: Modifier) {
+    Box(
+        modifier = modifier
+            .fillMaxHeight()
+            .fillMaxWidth(), contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Box 1")
+        Text(text = "Column 1")
+        Text(text = "Row 1")
+        Text(text = "Box 2")
+        Text(text = "Column 2")
+    }
+}
